65a30b1f6a90bc7eda0f84b8610ce3576610688d
Joomy Korkut <joomy@cattheory.com>
2019-01-07 14:43:21 -0500

    styling

--- openweb.js
+++ openweb.js
@@ -1,25 +1,20 @@
 // once page loads
 document.addEventListener('DOMContentLoaded', function() {
-	// identify the submit
-	var button = document.getElementById("clickme");
+  // identify the submit
+  var button = document.getElementById("clickme");
 
-	if (button) {
+  if (button) {
+    button.addEventListener("click", function() {
+      var url = document.getElementById("website").value.trim();
 
-		button.addEventListener("click", function(){
-			var url = document.getElementById("website").value.trim();
-
-			// open new webview with URL
-			chrome.app.window.create(
-     		   'webview.html',
-     		   {hidden: true},
-     		   function(win) {
-       				win.contentWindow.addEventListener('DOMContentLoaded',
-         		  		function(e) {
-		   		   		 	var webview = win.contentWindow.document.querySelector('webview');
-           					webview.src = url;
-           	 			   	win.show();
-       				});
-     		 });
-		 });
-	 }
-});
+      // open new webview with URL
+      chrome.app.window.create('webview.html', {hidden: true}, function(win) {
+        win.contentWindow.addEventListener('DOMContentLoaded', function(e) {
+            var webview = win.contentWindow.document.querySelector('webview');
+            webview.src = url;
+            win.show();
+        })
+      })
+    })
+  }
+})
--- sockets.js
+++ sockets.js
@@ -12,30 +12,30 @@ chrome.sockets.tcp.create(function(){});
 
 // show all sockets - works!
 chrome.sockets.tcp.getSockets(function(socketInfos) {
-        var d = {}
-        for (var i=0; i<socketInfos.length; i++) {
-            d[socketInfos[i].socketId] = socketInfos[i]
-        }
-        console.log('current tcp sockets', d);
-    });
-	
-	
-	
+  var d = {}
+  for (var i=0; i<socketInfos.length; i++) {
+    d[socketInfos[i].socketId] = socketInfos[i]
+  }
+  console.log('current tcp sockets', d);
+});
+
+
+
 /* EXAMPLES FOR WORKING WITH SOCKETS (https://developer.chrome.com/apps/app_network):
-	
+
 chrome.sockets.tcp.create({}, function(createInfo) {
   chrome.sockets.tcp.connect(createInfo.socketId,
     IP, PORT, onConnectedCallback);
 });
-	
+
 chrome.sockets.tcp.send(socketId, arrayBuffer, onSentCallback);
-	
+
 chrome.sockets.tcp.onReceive.addListener(function(info) {
   if (info.socketId != socketId)
     return;
   // info.data is an arrayBuffer.
 });
-	
+
 chrome.sockets.tcp.disconnect(socketId);
-	
-	*/
+
+*/
