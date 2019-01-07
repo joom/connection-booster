8aa4e0b2a6d017d80aa2590eb094a70c5cdc303c
Joomy Korkut <joomy@cattheory.com>
2019-01-07 18:16:05 -0500

    stuck state

--- openweb.js
+++ openweb.js
@@ -2,12 +2,24 @@
 document.addEventListener('DOMContentLoaded', function() {
   // identify the submit
   var button = document.getElementById("clickme");
+  var webview = document.querySelector('webview');
+  webview.src = `data:text/html,`
 
   if (button) {
     button.addEventListener("click", function() {
-      var url = document.getElementById("website").value.trim();
-      var webview = document.querySelector('webview');
-      webview.src = url;
+      // var url = document.getElementById("website").value.trim();
+      // webview.src = url
+      // webview.stop()
+      // console.log(webview.contentWindow)
+      webview.executeScript({code: `document.documentElement.innerHTML = "hey there"`}, function(results) {
+        // results[0] would have the webview's innerHTML.
+
+      });
     })
   }
+
+  // webview.addEventListener("contentload", function(load) {
+  //   console.log(load);
+  //   webview.stop()
+  // })
 })
