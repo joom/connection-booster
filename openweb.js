89d099eb5e58ac746f606be5ba6815ccfd43630a
Joomy Korkut <joomy@cattheory.com>
2019-01-08 15:54:16 -0500

    PROGRESS

--- openweb.js
+++ openweb.js
@@ -8,12 +8,18 @@ document.addEventListener('DOMContentLoaded', function() {
   if (button) {
     button.addEventListener("click", function() {
       var url = document.getElementById("website").value.trim();
-      webview.src = url
+      // webview.src = url
       // webview.stop()
       // console.log(webview.contentWindow)
-      // webview.executeScript({code: `document.documentElement.innerHTML = "hey there"`}, function(results) {
-      //   // results[0] would have the webview's innerHTML.
-      // });
+      socketMagic(url, (res) => {
+        console.log(res);
+        webview.src = "data:text/html," + res
+
+        // webview.executeScript({code: `document.documentElement.innerHTML`}, function(results) {
+        //   // results[0] would have the webview's innerHTML.
+        //   console.log(results);
+        // });
+      })
     })
   }
 
@@ -22,3 +28,34 @@ document.addEventListener('DOMContentLoaded', function() {
   //   webview.stop()
   // })
 })
+
+var socketMagic = (urlString, cb) => {
+  // make sure url starts with http://
+  var url = new URL(urlString)
+  if (url.protocol !== "http:") {
+    throw new Error("URL should start with http://")
+  }
+
+  var enc = new TextEncoder()
+  var dec = new TextDecoder()
+  var httpReqHeader = `GET ${url.pathname} HTTP/1.1\r\nHost: ${url.host}\r\n\r\n`
+  var httpReqHeaderEnc = enc.encode(httpReqHeader)
+
+  chrome.sockets.tcp.create({}, (createInfo) => {
+    var socketId = createInfo.socketId
+    chrome.sockets.tcp.connect(socketId, url.host, 80, (result) => {
+      chrome.sockets.tcp.send(socketId, httpReqHeaderEnc, (sendInfo) => {
+        chrome.sockets.tcp.onReceive.addListener((recvInfo) => {
+          // recvInfo.data is an arrayBuffer.
+          if (recvInfo.socketId != socketId) {
+            return
+          }
+          var res = dec.decode(recvInfo.data)
+          cb(res);
+          chrome.sockets.tcp.disconnect(socketId);
+        })
+      })
+    })
+  })
+
+}
