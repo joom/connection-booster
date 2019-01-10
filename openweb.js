279d650d6ac3a26ac619183165d8709c82deb974
Joomy Korkut <joomy@cattheory.com>
2019-01-10 17:10:39 -0500

    latest

--- openweb.js
+++ openweb.js
@@ -19,17 +19,18 @@ var parseHeaders = (res) => {
   return obj
 }
 
+var enc = new TextEncoder()
+var dec = new TextDecoder()
+
 // Takes a URL string, opens a TCP connection, sends an HTTP request, receives
 // an HTTP response and parses it. The callback function takes the response
 // body as a string, and the parsed object from the initial chunk.
 var socketMagic = (urlString, cb) => {
   var url = new URL(urlString)
   if (url.protocol !== "http:") {
-    throw new Error("URL should start with http://")
+    return
+    // throw new Error("URL should start with http://")
   }
-
-  var enc = new TextEncoder()
-  var dec = new TextDecoder()
   var httpReqHeader = `GET ${url.pathname} HTTP/1.1\r\nHost: ${url.host}\r\n\r\n`
   var httpReqHeaderEnc = enc.encode(httpReqHeader)
 
@@ -38,14 +39,15 @@ var socketMagic = (urlString, cb) => {
     chrome.sockets.tcp.connect(socketId, url.host, 80, (result) => {
       chrome.sockets.tcp.send(socketId, httpReqHeaderEnc, (sendInfo) => {
         var chunk = 0
-        all = ``
+        var all = ``
         var len = 0
         var totalLen = 0
         var obj = {}
 
         chrome.sockets.tcp.onReceive.addListener((recvInfo) => {
-          chunk++
           if (recvInfo.socketId != socketId) { return }
+          console.log(recvInfo)
+          chunk++
 
           // recvInfo.data is an arrayBuffer.
           var res = dec.decode(recvInfo.data)
@@ -63,6 +65,7 @@ var socketMagic = (urlString, cb) => {
           console.log(`Chunk ${chunk}, Len: ${len}, TotalLen: ${totalLen}`)
 
           if(len >= totalLen) {
+            console.log(obj)
             cb(all, obj)
             chrome.sockets.tcp.disconnect(socketId)
           }
@@ -81,7 +84,7 @@ var socketMagic = (urlString, cb) => {
 // and a callback that takes a document.
 var domMagic = (baseURL, body, mimeType, cb) => {
   var parser = new DOMParser()
-  doc = parser.parseFromString(body, mimeType)
+  var doc = parser.parseFromString(body, mimeType)
 
   // fully qualify all the links. i.e. resolve relative paths etc.
   doc.querySelectorAll("[src], [href]").forEach((elt) => {
@@ -99,17 +102,26 @@ var domMagic = (baseURL, body, mimeType, cb) => {
   var sourcemap = {}
 
   // gotta load images and stuff, i.e. socket shit
+  var parallelFns = []
   doc.querySelectorAll("img[src], script[src], link[href]").forEach((elt) => {
+    var addr = ``
+    if (elt.attributes.src) { addr = elt.attributes.src.value }
+    if (elt.attributes.href) { addr = elt.attributes.href.value }
+    if (sourcemap[addr]) { return } // don't download twice
 
-
+    parallelFns.push(function (callback) {
+      socketMagic(addr, (body, obj) => {
+        sourcemap[addr] = `data:${obj.mimeType},${body}`
+        callback(null, {})
+      })
+    })
   })
 
-
-  // style files from different resources
-
-  // scripts from different resources
-
-  cb(doc, sourcemap)
+  runParallel(parallelFns, function (err, results) {
+    // now all resources have been downloaded
+    console.log(sourcemap);
+    cb(doc, sourcemap)
+  })
 }
 
 // Takes a URL, does all the connection stuff and updates the WebView,
@@ -131,21 +143,37 @@ var urlMagic = (url, cb) => {
 }
 
 // Once page loads
-document.addEventListener("DOMContentLoaded", function() {
-  var button = document.getElementById("clickme")
+document.addEventListener("DOMContentLoaded", (ev) => {
+  var go = document.getElementById("go")
+  var clear = document.getElementById("clear")
   var webview = document.querySelector("webview")
   webview.src = `data:text/html,` // empty page
 
-  if (button) {
-    button.addEventListener("click", function() {
-      var url = document.getElementById("website").value.trim()
-      urlMagic(url, (doc) => {
-
-      })
-      // webview.src = url
-      // webview.stop()
-      // console.log(webview.contentWindow)
+  go.addEventListener("click", (ev) => {
+    var url = document.getElementById("website").value.trim()
+    urlMagic(url, (doc) => {
 
     })
-  }
+    // webview.src = url
+    // webview.stop()
+    // console.log(webview.contentWindow)
+
+  })
+
+  clear.addEventListener("click", (ev) => {
+    var clearDataType = {
+      appcache: true,
+      cache: true, // remove entire cache.
+      cookies: true,
+    }
+    webview.clearData({ since: 0 }, clearDataType, () => {
+    // Reload webview after clearing browsing data.
+      clear.disabled = true
+      clear.attributes.value.value = "Cleared!..."
+      setTimeout(() => {
+        clear.disabled = false
+        clear.attributes.value.value = "Clear cache"
+      }, 2000)
+    });
+  })
 })
---
+++ parallel.js
@@ -0,0 +1,91 @@
+// https://github.com/feross/run-parallel
+/*
+The MIT License (MIT)
+
+Copyright (c) Feross Aboukhadijeh
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+/* Example:
+
+parallel([
+  function (callback) {
+    setTimeout(function () {
+      callback(null, 'one')
+    }, 200)
+  },
+  function (callback) {
+    setTimeout(function () {
+      callback(null, 'two')
+    }, 100)
+  }
+],
+// optional callback
+function (err, results) {
+  // the results array will equal ['one','two'] even though
+  // the second function had a shorter timeout.
+})
+
+*/
+function runParallel (tasks, cb) {
+  var results, pending, keys
+  var isSync = true
+
+  if (Array.isArray(tasks)) {
+    results = []
+    pending = tasks.length
+  } else {
+    keys = Object.keys(tasks)
+    results = {}
+    pending = keys.length
+  }
+
+  function done (err) {
+    function end () {
+      if (cb) cb(err, results)
+      cb = null
+    }
+    if (isSync) process.nextTick(end)
+    else end()
+  }
+
+  function each (i, err, result) {
+    results[i] = result
+    if (--pending === 0 || err) {
+      done(err)
+    }
+  }
+
+  if (!pending) {
+    // empty
+    done(null)
+  } else if (keys) {
+    // object
+    keys.forEach(function (key) {
+      tasks[key](function (err, result) { each(key, err, result) })
+    })
+  } else {
+    // array
+    tasks.forEach(function (task, i) {
+      task(function (err, result) { each(i, err, result) })
+    })
+  }
+
+  isSync = false
+}
